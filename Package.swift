6e6712c79f90783ada8d5df879b711165b049595
Todd Heasley <toddheasley@me.com>
2020-07-18 15:35:59 -0400

    Bump Swiftt version to 5.3

--- Package.swift
+++ Package.swift
@@ -1,24 +1,18 @@
-// swift-tools-version:5.2
-
+// swift-tools-version:5.3
 import PackageDescription
 
-let package = Package(
-    name: "Version",
-    platforms: [
-        .macOS(.v10_15),
-        .iOS(.v13),
-        .watchOS(.v6),
-        .tvOS(.v13)
-    ],
-    products: [
-        .library(name: "Version", targets: [
-            "Version"
-        ])
-    ],
-    targets: [
-        .target(name: "Version", dependencies: []),
-        .testTarget(name: "VersionTests", dependencies: [
-            "Version"
-        ])
-    ]
-)
+let package = Package(name: "version", platforms: [
+    .macOS(.v10_15),
+    .iOS(.v13),
+    .watchOS(.v6),
+    .tvOS(.v13)
+], products: [
+    .library(name: "Version", targets: [
+        "Version"
+    ])
+], targets: [
+    .target(name: "Version", dependencies: []),
+    .testTarget(name: "VersionTests", dependencies: [
+        "Version"
+    ])
+])
